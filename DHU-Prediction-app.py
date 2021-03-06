151e6cf1620947656ca7a3424c1b930ec3dd723e
taseersuleman <75067547+taseersuleman@users.noreply.github.com>
2021-03-06 13:53:32 +0500

    Update DHU-Prediction-app.py

--- DHU-Prediction-app.py
+++ DHU-Prediction-app.py
@@ -14,8 +14,8 @@ st.title(
 )
 st.subheader("The tRNAmod is a web-server for the prediction of Dihydrouridine in transfer RNA (tRNA) modifications." 
              "Dihydrouridine is formed from the uridine base by reducing the carbon-carbon double bond at positions 5 and 6 as shown in Figure below."
-            "The current research study focused on the detection of D modification using a novel method for feature extraction from the RNA samples obtained from
-             "containing 1035 Positive Samples form three species including Homosapiens (525) , Mus musculus (435), and Saccharomyces Cerevisiae (75) and 1396 Negative Samples "
+            "The current research study focused on the detection of D modification using a novel method for feature extraction from the RNA samples obtained from"
+             "containing 1035 Positive Samples form three species including Homosapiens (525) , Mus musculus (435), and Saccharomyces Cerevisiae (75) and 1396 Negative Samples"
              "Statistical moments are incorporated for the extraction and representation of feature vectors based on the position as well as the composition of nucleotide bases.")
 
 #---------------------------------#
